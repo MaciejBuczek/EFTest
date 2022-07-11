163f50a2848da9840aadf8794a3fe9d3ae4a78f7
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-07-11 09:44:02 +0200

    Add project files.

---
+++ EFTest/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFTest
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TestModel> TestModels { get; set; }
+    }
+}
---
+++ EFTest/Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFTest.Models
+{
+    public class TestModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
